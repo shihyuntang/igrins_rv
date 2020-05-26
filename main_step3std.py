84f599ea309dde45af1f94eb372ce458203b3f26
sytang <sytang@lowell.edu>
2020-05-26 08:38:26 -0700

    testing...

--- main_step3std.py
+++ main_step3std.py
@@ -85,6 +85,11 @@ def rv_MPinst(label_t, chunk_ind, trk, i):
         print('  --> No A0-fitted template for night '+night+', skipping...')
         return nightsout, rvsminibox, parfitminibox, vsiniminibox
 
+    print(label_t)
+    print(label_t['0'])
+    print(np.unique(label_t['0']))
+    print(len( np.unique(label_t['0']) ))
+
     num_orders = len( np.unique(label_t['0']) )
     fits_layer = [ i for i in np.arange(num_orders)+1 if int(hdulist[i].columns[3].name[1:]) == int(order) ][0]
 
