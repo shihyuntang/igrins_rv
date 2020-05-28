7d46b966537691baa80c00c0f1eadc4dcf7c2ef4
astahl7 <33397762+astahl7@users.noreply.github.com>
2020-05-28 09:54:22 -0500

    Update main_step3std_A0template.py

--- main_step3std_A0template.py
+++ main_step3std_A0template.py
@@ -233,6 +233,19 @@ def rv_MPinst(label_t, chunk_ind, trk, i):
         #parfit_5 = optimizer(parfit_4,dpar_ip,fitobj,optimize)
         parfit = optimizer(parfit_4,dpar,fitobj,optimize)   # RV fitting
 
+        # if stellar template power is very low, throw out result
+        if parfit[1] < 0.1:
+            continue
+            
+        # if stellar or telluric template powers are exactly equal to their starting values, fit failed, throw out result
+        if parfit[1] == par_in[1] or parfit[3] == par_in[3]:
+            continue
+            
+        # if model dips below zero at any point, we're to close to edge of blaze, fit may be comrpomised, throw out result
+        smod,chisq = fmod(parfit,fitobj)
+        if len(smod[(smod < 0)]) > 0:
+            continue
+            
         if args.plotfigs == True:
             #outplotter(par_in, fitobj,'{}_{}_{}_par_in'.format(label,night,tag), trk, 0)
             outplotter(parfit, fitobj,'{}_{}_{}_parfit'.format(label,night,tag), trk, 0)
