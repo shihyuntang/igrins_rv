0489bfdb7ac1712d1ad3da94534fdcafc60fbb18
sytang <sytang@lowell.edu>
2020-06-01 22:46:16 -0700

    opt time 1200, set_xtol_rel 1e-8

--- Engine/opt.py
+++ Engine/opt.py
@@ -299,10 +299,10 @@ def optimizer(par0,dpar0, hardbounds_v_ip, fitobj, optimize):
             par0[5] = par0[5] + 1e-4
     opt.set_lower_bounds(lows)
     opt.set_upper_bounds(highs)
-    opt.set_maxtime(600) #seconds
+    opt.set_maxtime(1200) #seconds
     # Quit optimization based on relative change in output fit parameters between iterations.
     # Choosing smaller change tolerance than 1e-6 has demonstrated no improvement in precision.
-    opt.set_xtol_rel(1e-6)
+    opt.set_xtol_rel(1e-8)
     # parfit = opt.optimize(par0)
     try:
         parfit = opt.optimize(par0)
