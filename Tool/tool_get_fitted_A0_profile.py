290f7b2a1b9ccdd7e9cc4e55e1a6c334f20b3fe2
sytang <sytang@lowell.edu>
2020-06-05 14:28:20 -0700

    Update tool_get_fitted_A0_profile.py

--- Tool/tool_get_fitted_A0_profile.py
+++ Tool/tool_get_fitted_A0_profile.py
@@ -126,36 +126,36 @@ def rv_main(i, order0, order):
         tag = tagsnight[t]
         beam = beamsnight[t]
 
-        # if args.band=='K':
-        #     if order==11:
-        #         bound_cut = [200, 100]
-        #     elif order==12:
-        #         bound_cut = [900, 300]
-        #     elif order==13:
-        #         bound_cut = [200, 400]
-        #     elif order==14:
-        #         bound_cut = [150, 300]
-        #     else:
-        #         bound_cut = [150, 100]
-        # elif args.band=='H':
-        #     if order==10:
-        #         bound_cut = [250, 150]#ok
-        #     elif order==11:
-        #         bound_cut = [600, 150]
-        #     elif order==13:
-        #         bound_cut = [200, 600]#ok
-        #     elif order==14:
-        #         bound_cut = [700, 100]
-        #     elif order==16:
-        #         bound_cut = [400, 100]
-        #     elif order==17:
-        #         bound_cut = [1000, 100]
-        #     elif order==20:
-        #         bound_cut = [500, 150]
-        #     elif (order==7) or (order==8) or (order==9) or (order==12) or (order==15) or (order==18) or (order==19):
-        #         bound_cut = [500, 500]
-        #     else:
-        #         bound_cut = [150, 100]
+        if args.band=='K':
+            if order==11:
+                bound_cut = [200, 100]
+            elif order==12:
+                bound_cut = [900, 300]
+            elif order==13:
+                bound_cut = [200, 400]
+            elif order==14:
+                bound_cut = [150, 300]
+            else:
+                bound_cut = [150, 100]
+        elif args.band=='H':
+            if order==10:
+                bound_cut = [250, 150]#ok
+            elif order==11:
+                bound_cut = [600, 150]
+            elif order==13:
+                bound_cut = [200, 600]#ok
+            elif order==14:
+                bound_cut = [700, 100]
+            elif order==16:
+                bound_cut = [400, 100]
+            elif order==17:
+                bound_cut = [1000, 100]
+            elif order==20:
+                bound_cut = [500, 150]
+            elif (order==7) or (order==8) or (order==9) or (order==12) or (order==15) or (order==18) or (order==19):
+                bound_cut = [500, 500]
+            else:
+                bound_cut = [150, 100]
 
         x,wave,s,u = init_fitsread('{}{}/{}/'.format(inparam.inpath, night, beam),
                                     'target',
@@ -164,7 +164,7 @@ def rv_main(i, order0, order):
                                     order,
                                     tag,
                                     args.band,
-                                    [150, 150])
+                                    bound_cut)
 #-------------------------------------------------------------------------------
         s2n = s/u
         if np.nanmedian(s2n) < float(args.SN_cut):
