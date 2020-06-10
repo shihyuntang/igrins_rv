357042f9f43e0c723a210dc06f835b420b9bb268
sytang <sytang@lowell.edu>
2020-06-10 07:34:34 -0700

    minor changes

--- main_step2.py
+++ main_step2.py
@@ -232,10 +232,10 @@ def ini_MPinst(label_t, chunk_ind, trk, i):
                 hardbounds[0] = 0
             if hardbounds[3] < 0:
                 hardbounds[3] = 1
-            if hardbounds[4] < 0.1:
-                hardbounds[4] = 0.1
-            if hardbounds[5] > 0.9:
-                hardbounds[5] = 0.9
+            if hardbounds[4] < 0.05:
+                hardbounds[4] = 0.05
+            if hardbounds[5] > 0.95:
+                hardbounds[5] = 0.95
 
             cycles = 2
 
