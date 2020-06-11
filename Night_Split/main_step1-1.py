c7275577b9336b2b49137ac14bdaea1079147e02
sytang <sytang@lowell.edu>
2020-06-11 01:04:05 -0700

    Update main_step1-1.py

--- Night_Split/main_step1-1.py
+++ Night_Split/main_step1-1.py
@@ -45,16 +45,17 @@ def DataPrep(args, tar_night, tar_num, tar_frame, file_night_num, std_name, std_
         tag = '{:04d}'.format(tag0)
 
         try:
-            print('{}{}_{}/{}/SDC{}_{}_{}.spec.fits'.format(inpath, night, tag, frame, args.band, night, tag))
             hdulist = fits.open('{}{}_{}/{}/SDC{}_{}_{}.spec.fits'.format(inpath, night, tag, frame, args.band, night, tag))
+            print('{}{}_{}/{}/SDC{}_{}_{}.spec.fits'.format(inpath, night, tag, frame, args.band, night, tag))
             save_yn = 0
 
-        except FileNotFoundError:
-            try:
+        except:
+            temp_dir = 'SDC{}_{}_{}.spec.fits'.format(args.band, night, tag)
+            if temp_dir in os.lisdir('{}{}_{}/A'.format(inpath, night, tag_temp))
                 print('{}{}_{}/A/SDC{}_{}_{}.spec.fits'.format(inpath, night, tag_temp, args.band, night, tag))
                 hdulist = fits.open('{}{}_{}/{}/SDC{}_{}_{}.spec.fits'.format(inpath, night, tag_temp, frame, args.band, night, tag))
                 save_yn = 1
-            except:
+            else:
                 print('{}{}_{}/B/SDC{}_{}_{}.spec.fits'.format(inpath, night, tag_temp, args.band, night, tag))
                 hdulist = fits.open('{}{}_{}/{}/SDC{}_{}_{}.spec.fits'.format(inpath, night, tag_temp, frame, args.band, night, tag))
                 save_yn = 1
