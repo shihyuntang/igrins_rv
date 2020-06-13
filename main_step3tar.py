6768f5d6d462e84ed8ace2750f78c14dba3955f3
sytang <sytang@lowell.edu>
2020-06-13 12:44:47 -0700

    cyc 4

--- main_step3tar.py
+++ main_step3tar.py
@@ -255,7 +255,7 @@ def rv_MPinst(label_t, chunk_ind, trk, i):
 #        if args.plotfigs == True:#
 #            outplotter(targname,par_in,fitobj,'{}_{}_{}_1'.format(label,night,tag))
 
-        cycles = 2
+        cycles = 4
 
         optgroup = ['cont',
                     'wave',
