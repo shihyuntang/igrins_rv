123a58d32cd52e4e7c7d111acc8713ec920f00e8
astahl7 <33397762+astahl7@users.noreply.github.com>
2020-06-15 19:39:32 -0700

    Update main_step1.py

--- main_step1.py
+++ main_step1.py
@@ -542,7 +542,7 @@ if __name__ == '__main__':
     nightsFinal = np.array(list(sorted(set(A0data[ind]['night']))))
 
     if args.nights_use != '':
-        nightstemp = np.array(args.nights_use, dtype=np.int)
+        nightstemp = np.array(ast.literal_eval(args.nights_use), dtype=str)
         for nnn in nightstemp:
             if nnn not in nightsFinal:
                 sys.exit('NIGHT {} NOT FOUND UNDER ./Input_Data/{}'.format(nnn, args.targname))
