8c1a4535b1741b42b02f014617b21a3530f67da7
sytang <sytang@lowell.edu>
2020-06-17 16:50:56 -0700

    Update tool_pre_step1_get_IP.py

--- Tool/tool_pre_step1_get_IP.py
+++ Tool/tool_pre_step1_get_IP.py
@@ -513,7 +513,7 @@ if __name__ == '__main__':
         orders = np.append(np.arange(2, 9), np.array([10, 11, 12, 13, 14, 16]))
         #orders = [3]
     elif args.band=='H':
-        orders = np.append(np.arange(2, 7), np.array([10, 11, 13, 14, 16, 17, 20, 21, 22]))
+        orders = np.array([5, 6, 13, 14, 16, 21, 22])
 
     inparam = inparamsA0(inpath,outpath,args.plotfigs,tags,nightsFinal,humids,
                          temps,zds,press,obs,watm,satm,mwave0,mflux0,cdbs_loc,None,None)
