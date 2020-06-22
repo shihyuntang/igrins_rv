1169ba522c3e869cd1cb6f59da8c895e58237246
sytang <sytang@lowell.edu>
2020-06-21 23:10:21 -0700

    Update main_step0.py

--- main_step0.py
+++ main_step0.py
@@ -20,7 +20,7 @@ def DataPrep(args):
 
 #-------------------------------------------------------------------------------
 # Collect target star information
-    fileT = open('./Temp/Prepdata/Prepdata_targ_{}.txt'.format(args.targname), 'w')
+    fileT = open('./Input/Prepdata/Prepdata_targ_{}.txt'.format(args.targname), 'w')
     fileT.write('night beam tag mjd facility airmass bvc\n')
 
     nightsT = [];
@@ -63,7 +63,7 @@ def DataPrep(args):
     fileT.close()
 #-------------------------------------------------------------------------------
     ## Now collect A0 information
-    fileA0 = open('./Temp/Prepdata/Prepdata_A0_{}.txt'.format(args.targname), 'w')
+    fileA0 = open('./Input/Prepdata/Prepdata_A0_{}.txt'.format(args.targname), 'w')
     fileA0.write('night tag humid temp zd press obs airmass\n')
     noA0nights = []
 
