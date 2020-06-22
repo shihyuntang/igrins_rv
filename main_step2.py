b987ec8111e2966cbe525d9607ff6ffe8a7d621d
sytang <sytang@lowell.edu>
2020-06-22 15:20:10 -0700

    Update main_step2.py

--- main_step2.py
+++ main_step2.py
@@ -58,7 +58,7 @@ def ini_MPinst(args, inparam, orders, order_use, trk, i):
             num_orders += 1
         except:
             continue
-    print(np.arange(num_orders))
+
     # order in A0_treated.fits is no longer sequential...
     fits_layer = [ i for i in np.arange(num_orders)+1 if int(hdulist[i].columns[0].name[9:]) == order ][0]
 
@@ -165,7 +165,7 @@ def ini_MPinst(args, inparam, orders, order_use, trk, i):
         par9in = f[0]*1e4; par8in = f[1]*1e4; par7in = f[2]*1e4; par6in = f[3]*1e4;
         par[9] = par9in ;  par[8] = par8in ;  par[7] = par7in ;  par[6] = par6in
 
-        par[0] = initrvguess-inparam.bvcs[night+tag]
+        par[0] = initguesses-inparam.bvcs[night+tag]
         # Arrays defining parameter variations during optimization steps
         dpars1 = {'cont' : np.array([ 0.0, 0.0, 0.0, 0.0, 0.0,               0.0, 0.0,   0.0,  0.0,        0.,   1e7, 1, 1, 0,    0]),
                   'wave' : np.array([ 0.0, 0.0, 0.0, 0.0, 0.0,               0.0, 10.0,  10.0, 5.00000e-5, 0.,   0,   0, 0, 0,    0]),
