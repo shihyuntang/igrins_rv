08de4afccae4cb88e010cde50f26a1cc445243b8
sytang <sytang@lowell.edu>
2020-06-23 15:25:09 -0700

    update

--- main_step2.py
+++ main_step2.py
@@ -360,13 +360,13 @@ if __name__ == '__main__':
     print(u'''
 Input Parameters:
     Tartget             = {}
-    Filter              = \33[41m {} band  \033[0m
-    WaveLength file     = \33[41m WaveRegions_{}  \033[0m
-    S/N cut             > {}
-    Order Use           = \33[41m Order {}  \033[0m
-    Initial vsini       = \33[41m \33[41m {} km/s  \033[0m
-    vsini vary range    \u00B1 \33[41m {} km/s  \033[0m
-    RV initial guess    = \33[41m {} km/s  \033[0m
+    Filter              = \33[41m {} band \033[0m
+    WaveLength file     = \33[41m WaveRegions_{} \033[0m
+    S/N cut             > \33[41m {} \033[0m
+    Order Use           = \33[41m Order {} \033[0m
+    Initial vsini       = \33[41m {} km/s \033[0m
+    vsini vary range    \u00B1 \33[41m {} km/s \033[0m
+    RV initial guess    = \33[41m {} km/s \033[0m
     Stellar template use= \33[41m {} \033[0m
     Target Spectral Type= \33[41m {} \033[0m             <-------  [late K, M] recommended 'synthetic', [F, G, early K] SpTy recommended 'livingston'
     '''.format(args.targname, args.band, args.WRegion, args.SN_cut, args.label_use,
