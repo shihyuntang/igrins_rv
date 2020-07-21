fba6d373a31eac15a3ce72bdd9ce0490c106831f
astahl7 <33397762+astahl7@users.noreply.github.com>
2020-07-21 15:25:08 -0700

    Update macbro_double.py

--- Engine/macbro_double.py
+++ Engine/macbro_double.py
@@ -1,5 +1,5 @@
 
-def macbro_double(w,s,hwhmlist,hwhmlist2,plist,mulist):
+def macbro_double(w,s,hwhmlist,hwhmlist2,p,mu):
     
     nw = len(w) # points in spectrum
     dw_new = (w[-1] - w[0]) / (nw-1) #wavelength change per pixel
@@ -13,12 +13,11 @@ def macbro_double(w,s,hwhmlist,hwhmlist2,plist,mulist):
     npad = nhalf + 2 # pad pixels on each end
     wg = dw_min * (np.arange(ng,dtype=float) - (ng-1)/2.0) #wavelength scale of gaussian
     xg = (0.83255461 / hwhm_max) * wg  #convenient absisca
+    xg2 = (0.83255461 / hwhm_max) * (wg-p)  #convenient absisca
     
     spad = np.concatenate((s[0]*np.ones(npad),s,np.ones(npad)*s[-1]))
     hwhmpad = np.concatenate((hwhmlist[0]*np.ones(npad),hwhmlist,np.ones(npad)*hwhmlist[-1]))
     hwhm2pad = np.concatenate((hwhmlist2[0]*np.ones(npad),hwhmlist2,np.ones(npad)*hwhmlist2[-1]))
-    ppad = np.concatenate((plist[0]*np.ones(npad),plist,np.ones(npad)*plist[-1]))
-    mupad = np.concatenate((nlist[0]*np.ones(npad),nlist,np.ones(npad)*nlist[-1]))
     
     sout = np.zeros_like(spad)
     
@@ -29,9 +28,8 @@ def macbro_double(w,s,hwhmlist,hwhmlist2,plist,mulist):
         gpro = (dw_new/hwhm_new)*(hwhm_max/dw_min)*np.exp(-xg*xg*((hwhm_max/hwhm_new)*(dw_new/dw_min))**2)
         gpro = gpro / np.sum(gpro)
 
-        xg2 = (0.83255461 / hwhm_max) * (wg-ppad[t])  #convenient absisca
         gpro2 = (dw_new/hwhm_new2)*(hwhm_max/dw_min)*np.exp(-xg2*xg2*((hwhm_max/hwhm_new2)*(dw_new/dw_min))**2)
-        gpro2 = mupad[t]*gpro2 / np.sum(gpro2)
+        gpro2 = mu*gpro2 / np.sum(gpro2)
 
         
         gprotot = gpro + gpro2
