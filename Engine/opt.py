cdf801a071cb24936a06ecb1939b38c34c009d45
sytang <sytang@lowell.edu>
2020-07-29 15:06:43 -0700

    print fun update

--- Engine/opt.py
+++ Engine/opt.py
@@ -267,7 +267,7 @@ def fmod_conti(par,fitobj):
     return w, smod, cont, c2
 
 
-def optimizer(par0,dpar0, hardbounds_v_ip, fitobj, optimize):
+def optimizer(par0,dpar0, hardbounds_v_ip, fitobj, optimize, logger, night, order, tag):
     # NLopt convenience function.
     global fitobj_cp, optimize_cp, dpar0_cp
     fitobj_cp   = fitobj
@@ -294,7 +294,11 @@ def optimizer(par0,dpar0, hardbounds_v_ip, fitobj, optimize):
             par0[5] = par0[5] + 1e-4
     opt.set_lower_bounds(lows)
     opt.set_upper_bounds(highs)
-    opt.set_maxtime(1200) #seconds
+
+    maxruntime = 1200 #seconds
+    opt.set_maxtime(maxruntime) #seconds
+    if int(opt.get_maxtime()) == maxruntime:
+        logger.warning(f'  --> opt fit timeout, {night}_order{order}_{tag}')
     # Quit optimization based on relative change in output fit parameters between iterations.
     # Choosing smaller change tolerance than 1e-6 has demonstrated no improvement in precision.
     opt.set_xtol_rel(1e-8)
--- main_step3.py
+++ main_step3.py
@@ -245,7 +245,7 @@ def rv_MPinst(args, inparam, orders, order_use, trk, step2or3, i):
                 parstart = par_in.copy()
 
             for optkind in optgroup:
-                parfit_1 = optimizer(parstart, dpars[optkind], hardbounds, fitobj, optimize)
+                parfit_1 = optimizer(parstart, dpars[optkind], hardbounds, fitobj, optimize, logger, night, order, tag)
                 parstart = parfit_1.copy()
                 if args.debug == True:
                     outplotter_23(parfit_1,fitobj,'{}_{}_{}_parfit_{}{}'.format(order,night,tag,nk,optkind), trk, inparam, args, step2or3)
