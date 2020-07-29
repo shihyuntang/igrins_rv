b87773d0eb62d6f31650b47c9d55815387b66ca5
sytang <sytang@lowell.edu>
2020-07-29 15:06:43 -0700

    print fun update

--- main_step2.py
+++ main_step2.py
@@ -249,7 +249,7 @@ def ini_MPinst(args, inparam, orders, order_use, trk, step2or3, i):
             dpars = dpars2
 
         for optkind in optgroup:
-            parfit_1 = optimizer( parstart, dpars[optkind], hardbounds, fitobj, optimize)
+            parfit_1 = optimizer( parstart, dpars[optkind], hardbounds, fitobj, optimize, logger, night, order, tag)
             parstart = parfit_1.copy()
             if args.debug:
                 outplotter_23(parfit_1, fitobj, '{}_{}_{}_parfit_{}{}'.format(order,night,tag,nk,optkind), trk, inparam, args, step2or3)
