d9befcc725ee2461be1b7dc6b1ec0eda63538a0a
sytang <sytang@lowell.edu>
2020-07-30 09:15:42 -0700

    Update main_step3.py

--- main_step3.py
+++ main_step3.py
@@ -245,6 +245,7 @@ def rv_MPinst(args, inparam, orders, order_use, trk, step2or3, i):
                 parstart = par_in.copy()
 
             for optkind in optgroup:
+                print(f'{optkind}, nc={nc}, tag={tag}')
                 parfit_1 = optimizer(parstart, dpars[optkind], hardbounds, fitobj, optimize, logger, night, order, tag, optkind, nc, nk)
                 parstart = parfit_1.copy()
                 if args.debug == True:
