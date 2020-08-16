f82aef8112182bdf72976633fac9647c7aaf5864
sytang <sytang@lowell.edu>
2020-08-16 08:33:52 -0700

    logger info added

--- main_step4.py
+++ main_step4.py
@@ -37,6 +37,8 @@ if __name__ == '__main__':
     parser.add_argument("-HorK",    dest="band",             action="store",
                         help="Which band to process? H or K?. Default = K",
                         type=str,   default='K')
+    parser.add_argument('-sk_check', dest="skip",           action="store_true",
+                        help="If set, will skip the input parameters check. Handy when running mutiple targets line by line")
     parser.add_argument('--version',                          action='version',  version='%(prog)s 0.9')
     args = parser.parse_args()
     inpath   = './Input/{}/'.format(args.targname)
@@ -49,6 +51,8 @@ if __name__ == '__main__':
     if args.run1 == '' or args.run2 == '':
         sys.exit('ERROR: REQUIRES TWO RUNS SPECIFIED THROUGH -run1 AND -run2 FLAGS!')
 
+
+    #-------------------------------------------------------------------------------
     start_time = datetime.now()
     print('\n')
     print('####################################################################################\n')
@@ -60,15 +64,16 @@ Input Parameters:
     Run 1               = \33[41m RV_results_{} \033[0m   <------- Should be run where vsini = best guess
     Run 2               = \33[41m RV_results_{} \033[0m   <------- Should be run where vsini = best guess +- sigma
     '''.format(args.targname, args.band, args.run1,args.run2))
-    while True:
-        inpp = input("Press [Y]es to continue, [N]o to quite...\n --> ")
-        if 'n' in inpp.lower():
-            sys.exit('QUIT, PLEASE RE-ENTER YOUR PARAMETERS')
-        elif 'y' in inpp.lower():
-            break
-        else:
-            print('I cannot understand what you are saying... TRY AGAIN')
-            continue
+    if not args.skip:
+        while True:
+            inpp = input("Press [Y]es to continue, [N]o to quite...\n --> ")
+            if 'n' in inpp.lower():
+                sys.exit('QUIT, PLEASE RE-ENTER YOUR PARAMETERS')
+            elif 'y' in inpp.lower():
+                break
+            else:
+                print('I cannot understand what you are saying... TRY AGAIN')
+                continue
 
     print('---------------------------------------------------------------')
     print('Running Step 4 for {}...'.format(args.targname))
@@ -76,31 +81,55 @@ Input Parameters:
     #-------------------------------------------------------------------------------
 
     name = f'RV_results_{args.run1}_{args.run2}_combined'
-    os.mkdir(f'./Output/{args.targname}_{args.band}/{name}')
+
+    if not os.path.isdir(f'./Output/{args.targname}_{args.band}/{name}'):
+        os.mkdir(f'./Output/{args.targname}_{args.band}/{name}')
+    else:
+        sys.exit(f'OOPS! \"./Output/{args.targname}_{args.band}/{name}\" ALREADY EXIST!!')
 
     outpath = f'./Output/{args.targname}_{args.band}'
+    #-------------------------------------------------------------------------------
+    # Set up logger
+    logger = logging.getLogger(__name__)
+    if args.debug:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s: %(module)s.py: %(levelname)s--> %(message)s')
+
+    file_hander  = logging.FileHandler(f'{outpath}/{args.targname}_{args.band}.log')
+    stream_hander= logging.StreamHandler()
+
+    # file_hander.setLevel()
+    file_hander.setFormatter(formatter)
+
+    logger.addHandler(file_hander)
+    logger.addHandler(stream_hander)
+
+    logger.info(f'Writing output to ./Output/{args.targname}_{args.band}/{name}')
+    #-------------------------------------------------------------------------------
 
     rvses = []
 
-    for run in [args.run2,args.run1]:
-
-        hdu = fits.open('{}/{}/RVresultsSummary.fits'.format(outpath, f'RV_results_{run}'))
+    for run in [args.run2, args.run1]:
+        hdu    = fits.open('{}/{}/RVresultsSummary.fits'.format(outpath, f'RV_results_{run}'))
         tbdata = hdu[1].data
         rvses.append(np.array(tbdata['RVfinal'],dtype=float))
-        stds = np.array(tbdata['STDfinal'],dtype=float)
-        jdsCombined = np.array(tbdata['JD'],dtype=float)
-        nightsCombined = np.array(tbdata['NIGHT'],dtype=str)
-        vsinifinalCombined = np.array(tbdata['VSINI'],dtype=float)
+        stds               = np.array(tbdata['STDfinal'], dtype=float)
+        jdsCombined        = np.array(tbdata['JD'],       dtype=float)
+        nightsCombined     = np.array(tbdata['NIGHT'],    dtype=str)
+        vsinifinalCombined = np.array(tbdata['VSINI'],    dtype=float)
 
-    vsini_err = np.nanstd(rvses[0]-rvses[1])
+    vsini_err        = np.nanstd(rvses[0]-rvses[1])
     stdfinalCombined = np.sqrt(stds**2 + vsini_err**2)
-    rvfinalCombined = rvses[1]
+    rvfinalCombined  = rvses[1]
 
     intnights = np.array([int(i[:8]) for i in nightsCombined])
-    indT = np.where((intnights < 20180401) | (intnights > 20190531))
+    indT = np.where((intnights <  20180401) | (intnights > 20190531))
     indL = np.where((intnights >= 20180401) & (intnights < 20190531))
     nightsT = nightsCombined[indT]
     nightsL = nightsCombined[indL]
+    #-------------------------------------------------------------------------------
 
     # Plot combined results
     xscale = np.arange(len(rvfinalCombined))+1
@@ -131,10 +160,11 @@ Input Parameters:
     axes.yaxis.set_minor_locator(AutoMinorLocator(5))
     axes.tick_params(axis='both', which='both', labelsize=6, right=True, top=True, direction='in', width=.6)
     f.savefig('{}/{}/FinalRVs.png'.format(outpath, name), format='png', bbox_inches='tight')
+    #-------------------------------------------------------------------------------
 
     # Output combined final results to fits file
     c1 = fits.Column(name='NIGHT',    array=nightsCombined,         format='{}A'.format(len(nightsCombined[0])) )
-    c2 = fits.Column(name='JD',       array=jdsCombined,           format='D')
+    c2 = fits.Column(name='JD',       array=jdsCombined,            format='D')
     c3 = fits.Column(name='RVfinal',  array=rvfinalCombined,        format='D')
     c4 = fits.Column(name='STDfinal', array=stdfinalCombined,       format='D')
     c5 = fits.Column(name='VSINI',    array=vsinifinalCombined,     format='D')
@@ -149,11 +179,12 @@ Input Parameters:
 
     tempin = Table.read('{}/{}/RVresultsSummary.fits'.format(outpath, name), format='fits')
     tempin.write('{}/{}/RVresultsSummary.csv'.format(outpath, name), format='csv', overwrite=True)
+    #-------------------------------------------------------------------------------
 
-    print('Combined RV results: mean={:1.4f} km/s, std={:1.4f} km/s'.format(np.nanmean(rvfinalCombined),
-                                                                            np.nanstd(rvfinalCombined)))
+    logger.info('Combined RV results: mean={:1.4f} km/s, std={:1.4f} km/s'.format(np.nanmean(rvfinalCombined),
+                                                                                  np.nanstd(rvfinalCombined)))
     print('\n')
     end_time = datetime.now()
-    print('Whole process DONE!!!!!!, Duration: {}'.format(end_time - start_time))
-    print('Output saved under {}/{}'.format(outpath, name) )
+    logger.info('Whole process DONE!!!!!!, Duration: {}'.format(end_time - start_time))
+    logger.info('Output saved under {}/{}'.format(outpath, name) )
     print('####################################################################################')
