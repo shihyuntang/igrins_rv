3d9a06b7ed6884f58c1c77b2782f73795d0fcd1c
sytang <sytang@lowell.edu>
2020-09-12 15:36:22 -0700

    Update main_step2.py

--- main_step2.py
+++ main_step2.py
@@ -243,7 +243,8 @@ def ini_MPinst(args, inparam, orders, order_use, trk, step2or3, i):
                     'twave',
                     'ip', 'v',
                     'ip', 'v',
-                    'twave',  's']
+                    'twave',  's',
+                    'cont'  's']
 
     nk = 1
     for nc, cycle in enumerate(np.arange(cycles), start=1):
