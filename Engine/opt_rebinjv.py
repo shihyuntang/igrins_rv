3829e2197229720ee7ec7b5ac88681cb4f6f1028
sytang <sytang@lowell.edu>
2020-09-14 15:33:58 -0700

    Update opt_rebinjv.py

--- Engine/opt_rebinjv.py
+++ Engine/opt_rebinjv.py
@@ -39,7 +39,7 @@ def fmodel_chi(par,grad):
     # Can't call these directly in function, as NLopt doesn't allow anything to be in the model function call besides par and grad.
 
     #global fitobj, optimize
-    global fitobj_cp, optimize_cp, trksy_cp#, dpar0_cp, optkind_cp, nk_cp, nc_cp
+    global fitobj_cp, optimize_cp#, trksy_cp#, dpar0_cp, optkind_cp, nk_cp, nc_cp
 
     watm = fitobj_cp.watm_in;
     satm = fitobj_cp.satm_in;
@@ -126,8 +126,8 @@ def fmodel_chi(par,grad):
     chisq = np.sum((fitobj_cp.s[mask] - smod[mask])**2. / fitobj_cp.u[mask]**2.)
     chisq = chisq / (len(smod[mask]) - 15)
 #------- sy test chi2
-    filechi2 = open(f'./Output/TauBoo_H/20160225_0109_opt{trksy_cp}.csv', 'a')
-    filechi2.write(f'{par[0]}, {chisq}\n')
+    # filechi2 = open(f'./Output/TauBoo_H/20160225_0109_opt{trksy_cp}.csv', 'a')
+    # filechi2.write(f'{par[0]}, {chisq}\n')
 #------- sy test chi2
 
     # if (dpar0_cp[0] == 0) & (dpar0_cp[11] != 0):
@@ -285,10 +285,10 @@ def fmod_conti(par,fitobj):
 # def optimizer(par0,dpar0, hardbounds_v_ip, fitobj, optimize, logger, night, order, tag, optkind, nc, nk):
 def optimizer(par0,dpar0, hardbounds_v_ip, fitobj, optimize):
     # NLopt convenience function.
-    global fitobj_cp, optimize_cp, trksy_cp#, dpar0_cp, optkind_cp, nk_cp, nc_cp
+    global fitobj_cp, optimize_cp#, trksy_cp#, dpar0_cp, optkind_cp, nk_cp, nc_cp
     fitobj_cp   = fitobj
     optimize_cp = optimize
-    trksy_cp = trksy
+    #trksy_cp = trksy
     # dpar0_cp = dpar0
     # optkind_cp = optkind
     # nk_cp = nk
