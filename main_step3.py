75c435619455375406f1d23251bb7f03358a5bbc
sytang <sytang@lowell.edu>
2020-09-14 15:26:18 -0700

    Update main_step3.py

--- main_step3.py
+++ main_step3.py
@@ -233,20 +233,20 @@ def rv_MPinst(args, inparam, orders, order_use, trk, step2or3, i):
 
         cycles = 4
 
-        # optgroup = ['cont', 'twave', 'cont', 's',
-        #             'cont', 'twave', 's', 'cont',
-        #             'twave',
-        #             'ip', 'v',
-        #             'ip', 'v',
-        #             'twave',  's',
-        #             'twave',  's']
-
         optgroup = ['cont', 'twave', 'cont', 's',
                     'cont', 'twave', 's', 'cont',
                     'twave',
                     'ip', 'v',
                     'ip', 'v',
-                    'twave', 's']
+                    'twave',  's',
+                    'twave',  's']
+
+        # optgroup = ['cont', 'twave', 'cont', 's',
+        #             'cont', 'twave', 's', 'cont',
+        #             'twave',
+        #             'ip', 'v',
+        #             'ip', 'v',
+        #             'twave', 's']
 
         nk = 1
         for nc, cycle in enumerate(np.arange(cycles), start=1):
