5debedb96fddb1efd8a6f21b3da557b876bb7d06
astahl7 <33397762+astahl7@users.noreply.github.com>
2020-10-08 09:52:51 -0500

    Update main_step1.py

--- main_step1.py
+++ main_step1.py
@@ -141,8 +141,8 @@ def MPinst(args, inparam, jerp, orders, i):
                   par_in[5] -dpar[5],   par_in[5]+dpar[5]]
     if hardbounds[0] < 0:
         hardbounds[0] = 0
-    if hardbounds[3] < 0:
-        hardbounds[3] = 1
+    if hardbounds[2] < 0:
+        hardbounds[2] = 1
 
     # Begin optimization.
     # For every pre-Telfit spectral fit, first fit just template strength/rv/continuum, then just wavelength solution, then template/continuum again, then ip,
