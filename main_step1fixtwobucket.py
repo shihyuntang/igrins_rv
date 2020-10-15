67ffbea53a5eb3661112bfbcd69015855df992ab
sytang <sytang@lowell.edu>
2020-10-15 15:59:54 -0700

    Update main_step1fixtwobucket.py

--- main_step1fixtwobucket.py
+++ main_step1fixtwobucket.py
@@ -89,7 +89,7 @@ def MPinst(args, inparam, jerp, orders, masterbeam, i):
     a0u        = basicclip_above(a0u,a0fluxlist,nzones);   a0fluxlist = basicclip_above(a0fluxlist,a0fluxlist,nzones);
 
     # Normalize
-    a0fluxlist /= np.median(a0fluxlist)
+    # a0fluxlist /= np.median(a0fluxlist)
 
     # Compute rough blaze function estimate. Better fit will be provided by Telfit later.
     continuum    = A0cont(a0wavelist,a0fluxlist,night,order)
