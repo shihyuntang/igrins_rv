29dfb4ef81db7c36becf5ca066ed68b4a79fbba4
sytang <sytang@lowell.edu>
2020-10-17 10:35:22 -0700

    Update outplotterwithtwodip.py

--- Engine/outplotterwithtwodip.py
+++ Engine/outplotterwithtwodip.py
@@ -125,69 +125,69 @@ def outplotter_23(parfit, fitobj, title, trk, inparam, args, step2or3):
     fig.savefig(f'{inparam.outpath}/figs/main_step{step2or3}_{args.band}_{trk}/{title}.png', bbox_inches='tight', format='png', overwrite=True)
 
 
-    fig, axes = plt.subplots(1, 1, figsize=(6,3), facecolor='white', dpi=300)
-
-    n = len(fitobj.mask)
-
-    if n > 0:
-        widths = [fitobj.mask[0][0]-fitobj.x[0]]
-        for m in range(n-1):
-            widths.append(fitobj.mask[m+1][0]-fitobj.mask[m][1])
-        widths.append(fitobj.x[-1]-fitobj.mask[n-1][1])
-        gs = gridspec.GridSpec(1, n+1, width_ratios=widths)
-        for m in range(n+1):
-            ax0 = plt.subplot(gs[m])
-            ax0.plot(fitobj.x,fitobj.s, '-',  c='k',       lw=0.7, label='data',  alpha=.6)
-            ax0.plot(fitobj.x,fit,      '--', c='tab:red', lw=0.7, label='model', alpha=.6)
-            ax0.plot(fitobj.x,cont,     '--', c='tab:blue',  lw=0.7, label='cont', alpha=.6)
-            kwargs = dict(transform=ax0.transAxes, color='k', clip_on=False,lw= 0.6)
-            if m == 0:
-                ax0.tick_params(axis='both', labelsize=6, right=False, top=True, direction='in')
-                left = fitobj.x[0]
-                right = fitobj.mask[m][0]
-                ax0.plot([right,right],[min(fitobj.s),max(fitobj.s)],'--k',lw=0.75)
-            elif m == n:
-                ax0.tick_params(axis='both', labelsize=6, left=False, right=True, top=True, direction='in')
-                ax0.set_yticklabels([])
-                #ax0.vlines([left+1],'--k',lw=0.75)
-                left = fitobj.mask[m-1][1]
-                right = fitobj.x[-1]
-            else:
-                ax0.tick_params(axis='both', labelsize=6, right=False, left=False,top=True, direction='in')
-                ax0.set_yticklabels([])
-                #ax0.vlines([left+1],'--k',lw=0.75)
-                ax0.plot([right,right],[min(fitobj.s),max(fitobj.s)],'--k',lw=0.75)
-                left = fitobj.mask[m-1][1]
-                right = fitobj.mask[m][0]
-
-            ax0.set_xlim(left,right)
-
-            if m != 0:
-                ax0.spines['left'].set_visible(False)
-            if m != n:
-                ax0.spines['right'].set_visible(False)
-
-        fig.tight_layout(pad=0.0)
-        fig.suptitle( title,     x=0.5,y=1.05, size=6, style='normal', family='sans-serif')
-        fig.text(0.5, -0.04, r'Pixel', ha='center', size=6, style='normal', family='sans-serif')
-        fig.text(-0.04, 0.5, r'Flux',       va='center', rotation='vertical', size=6, style='normal', family='sans-serif')
-        ax0.legend(fontsize=5, edgecolor='white')
-
-    else:
-        fig, axes = plt.subplots(1, 1, figsize=(6,3), facecolor='white', dpi=300)
-        axes.plot(fitobj.x,fitobj.s, '-',  c='k',       lw=0.7, label='data',  alpha=.6)
-        axes.plot(fitobj.x,fit,      '--', c='tab:red', lw=0.7, label='model', alpha=.6)
-        axes.plot(fitobj.x,cont,     '--', c='tab:blue',  lw=0.7, label='cont', alpha=.6)
-
-        axes.tick_params(axis='both', labelsize=6, right=True, top=True, direction='in')
-        axes.set_title(title,  size=6, style='normal' , family='sans-serif' )
-        axes.set_ylabel(r'Flux',        size=6, style='normal', family='sans-serif' )
-        axes.set_xlabel(r'Pixel',  size=6, style='normal', family='sans-serif' )
-
-        axes.tick_params(axis='both', labelsize=6, right=True, top=True, direction='in')
-        axes.legend(fontsize=5, edgecolor='white')
-
-    fig.savefig(f'{inparam.outpath}/figs/main_step{step2or3}_{args.band}_{trk}/{title}_X.png', bbox_inches='tight', format='png', overwrite=True)
+    # fig, axes = plt.subplots(1, 1, figsize=(6,3), facecolor='white', dpi=300)
+    #
+    # n = len(fitobj.mask)
+    #
+    # if n > 0:
+    #     widths = [fitobj.mask[0][0]-fitobj.x[0]]
+    #     for m in range(n-1):
+    #         widths.append(fitobj.mask[m+1][0]-fitobj.mask[m][1])
+    #     widths.append(fitobj.x[-1]-fitobj.mask[n-1][1])
+    #     gs = gridspec.GridSpec(1, n+1, width_ratios=widths)
+    #     for m in range(n+1):
+    #         ax0 = plt.subplot(gs[m])
+    #         ax0.plot(fitobj.x,fitobj.s, '-',  c='k',       lw=0.7, label='data',  alpha=.6)
+    #         ax0.plot(fitobj.x,fit,      '--', c='tab:red', lw=0.7, label='model', alpha=.6)
+    #         ax0.plot(fitobj.x,cont,     '--', c='tab:blue',  lw=0.7, label='cont', alpha=.6)
+    #         kwargs = dict(transform=ax0.transAxes, color='k', clip_on=False,lw= 0.6)
+    #         if m == 0:
+    #             ax0.tick_params(axis='both', labelsize=6, right=False, top=True, direction='in')
+    #             left = fitobj.x[0]
+    #             right = fitobj.mask[m][0]
+    #             ax0.plot([right,right],[min(fitobj.s),max(fitobj.s)],'--k',lw=0.75)
+    #         elif m == n:
+    #             ax0.tick_params(axis='both', labelsize=6, left=False, right=True, top=True, direction='in')
+    #             ax0.set_yticklabels([])
+    #             #ax0.vlines([left+1],'--k',lw=0.75)
+    #             left = fitobj.mask[m-1][1]
+    #             right = fitobj.x[-1]
+    #         else:
+    #             ax0.tick_params(axis='both', labelsize=6, right=False, left=False,top=True, direction='in')
+    #             ax0.set_yticklabels([])
+    #             #ax0.vlines([left+1],'--k',lw=0.75)
+    #             ax0.plot([right,right],[min(fitobj.s),max(fitobj.s)],'--k',lw=0.75)
+    #             left = fitobj.mask[m-1][1]
+    #             right = fitobj.mask[m][0]
+    #
+    #         ax0.set_xlim(left,right)
+    #
+    #         if m != 0:
+    #             ax0.spines['left'].set_visible(False)
+    #         if m != n:
+    #             ax0.spines['right'].set_visible(False)
+    #
+    #     fig.tight_layout(pad=0.0)
+    #     fig.suptitle( title,     x=0.5,y=1.05, size=6, style='normal', family='sans-serif')
+    #     fig.text(0.5, -0.04, r'Pixel', ha='center', size=6, style='normal', family='sans-serif')
+    #     fig.text(-0.04, 0.5, r'Flux',       va='center', rotation='vertical', size=6, style='normal', family='sans-serif')
+    #     ax0.legend(fontsize=5, edgecolor='white')
+    #
+    # else:
+    #     fig, axes = plt.subplots(1, 1, figsize=(6,3), facecolor='white', dpi=300)
+    #     axes.plot(fitobj.x,fitobj.s, '-',  c='k',       lw=0.7, label='data',  alpha=.6)
+    #     axes.plot(fitobj.x,fit,      '--', c='tab:red', lw=0.7, label='model', alpha=.6)
+    #     axes.plot(fitobj.x,cont,     '--', c='tab:blue',  lw=0.7, label='cont', alpha=.6)
+    #
+    #     axes.tick_params(axis='both', labelsize=6, right=True, top=True, direction='in')
+    #     axes.set_title(title,  size=6, style='normal' , family='sans-serif' )
+    #     axes.set_ylabel(r'Flux',        size=6, style='normal', family='sans-serif' )
+    #     axes.set_xlabel(r'Pixel',  size=6, style='normal', family='sans-serif' )
+    #
+    #     axes.tick_params(axis='both', labelsize=6, right=True, top=True, direction='in')
+    #     axes.legend(fontsize=5, edgecolor='white')
+    #
+    # fig.savefig(f'{inparam.outpath}/figs/main_step{step2or3}_{args.band}_{trk}/{title}_X.png', bbox_inches='tight', format='png', overwrite=True)
 
 
 
