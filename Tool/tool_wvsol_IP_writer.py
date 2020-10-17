718957757555cd2046b9cce6a5257cd659d925a1
astahl7 <33397762+astahl7@users.noreply.github.com>
2020-10-16 19:12:28 -0500

    Update tool_wvsol_IP_writer.py

--- Tool/tool_wvsol_IP_writer.py
+++ Tool/tool_wvsol_IP_writer.py
@@ -105,10 +105,20 @@ def IPval(tar,band,args):
             filew.write(f'Tight {nodd}\n')
             for order in list(sorted(ipmaster.keys())):
                 xorder = np.arange(xbounddict[order][0],xbounddict[order][1])
+                
+                plt.figure(figsize=(12,12))
+                for i in range(len(ipmaster[order][:,0])):
+                    plt.plot(xorder,ipmaster[order][i,:],alpha=0.5,color='black')
                 ipmedian = [np.median(ipmaster[order][:,i]) for i in range(len(ipmaster[order][0,:]))]
+                plt.plot(xorder,ipmedian,alpha=0.75,color='red')
 
                 f = np.polyfit(xorder,ipmedian,2)
                 q = np.poly1d(f)
+                
+                plt.plot(xorder,q(xorder),alpha=0.75,color='blue')
+                plt.savefig('Tight_{}_IPs_{}.png'.format(nodd,order))
+                plt.clf()
+                plt.close()
 
                 filew.write('{}: np.array([{:+1.10f}, {:+1.10f}, {:+1.10f}]),\n'.format(order, q[2], q[1], q[0] ))
 
@@ -143,10 +153,20 @@ def IPval(tar,band,args):
             filew.write(f'Loose {nodd}\n')
             for order in list(sorted(ipmaster.keys())):
                 xorder = np.arange(xbounddict[order][0],xbounddict[order][1])
+                
+                plt.figure(figsize=(12,12))
+                for i in range(len(ipmaster[order][:,0])):
+                    plt.plot(xorder,ipmaster[order][i,:],alpha=0.5,color='black')
                 ipmedian = [np.median(ipmaster[order][:,i]) for i in range(len(ipmaster[order][0,:]))]
+                plt.plot(xorder,ipmedian,alpha=0.75,color='red')
 
                 f = np.polyfit(xorder,ipmedian,2)
                 q = np.poly1d(f)
+                
+                plt.plot(xorder,q(xorder),alpha=0.75,color='blue')
+                plt.savefig('Loose_{}_IPs_{}.png'.format(nodd,order))
+                plt.clf()
+                plt.close()
 
                 filew.write('{}: np.array([{:+1.10f}, {:+1.10f}, {:+1.10f}]),\n'.format(order, q[2], q[1], q[0] ))
 
