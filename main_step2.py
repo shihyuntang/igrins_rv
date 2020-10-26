7e21a3f62738767b334f553ab838e87e7e3bdf35
sytang <sytang@lowell.edu>
2020-10-25 20:21:19 -0700

    update boundary cut

--- main_step2.py
+++ main_step2.py
@@ -164,13 +164,13 @@ def rv_MPinst(args, inparam, orders, order_use, trk, step2or3, i):
     # Retrieve pixel bounds for where within each other significant telluric absorption is present.
     # If these bounds were not applied, analyzing some orders would give garbage fits.
     if args.band=='K':
-        if int(order) in [14]:
+        if int(order) in [13, 14]:
             bound_cut = inparam.bound_cut_dic[args.band][order]
         else:
             bound_cut = [150, 150]
 
     elif args.band=='H':
-        if int(order) in [13, 14, 16, 20]:
+        if int(order) in [6, 10, 11, 13, 14, 16, 17, 20, 21, 22]:
             bound_cut = inparam.bound_cut_dic[args.band][order]
         else:
             bound_cut = [150, 150]
