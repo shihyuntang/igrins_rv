71413bf5e58c0c81f4464eebab01b7fc45200bb7
astahl7 <33397762+astahl7@users.noreply.github.com>
2020-12-21 14:47:48 -0600

    Update tool_preStep1_getIP.py

--- Tool/tool_preStep1_getIP.py
+++ Tool/tool_preStep1_getIP.py
@@ -55,7 +55,7 @@ def MPinst(args, inparam, jerp, orders, i):
     ### Load relevant A0 spectrum
     x, a0wavelist, a0fluxlist, u = init_fitsread(inparam.inpath,
                                                  'A0',
-                                                 'combined',
+                                                 'separate',
                                                  night,
                                                  order,
                                                  f'{int(inparam.tags[night]):04d}',
