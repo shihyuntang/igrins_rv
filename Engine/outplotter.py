e448da5c03c40e0edc2f7ce5557a8e6e055dc456
astahl7 <33397762+astahl7@users.noreply.github.com>
2021-08-05 14:39:02 -0500

    Update outplotter.py

--- Engine/outplotter.py
+++ Engine/outplotter.py
@@ -106,19 +106,28 @@ def outplotter_23(parfit, fitobj, title, trk, inparam, args, step2or3, order):
 
     fit,chi = fmod(parfit, fitobj)
 
+    w = parfit[6] + parfit[7]*fitobj.x + parfit[8]*(fitobj.x**2.) + parfit[9]*(fitobj.x**3.)
+
+    dstep = np.median(w[1:]-w[:-1])
+    nstep = int((w[-1]-w[0])/dstep)
+    wreg = np.linspace(w[0],w[-1],nstep)
+    sdata = rebin_jv(w,fitobj.s,wreg,False)
+    udata = rebin_jv(w,fitobj.u,wreg,False)
+    xdata = np.linspace(fitobj.x[0],fitobj.x[-1],nstep)
+    w = wreg.copy()
+
     npars = len(parfit)
 
-    mask = np.ones_like(fitobj.s,dtype=bool)
-    mask[(fitobj.s < .0)] = False
+    mask = np.ones_like(sdata,dtype=bool)
+    mask[(sdata < .0)] = False
 
     if len(fitobj.mask) != 0:
         for maskbounds in fitobj.mask:
-            mask[(fitobj.x > maskbounds[0]) & (fitobj.x < maskbounds[1]) ] = False
+            mask[(xdata > maskbounds[0]) & (xdata < maskbounds[1]) ] = False
 
     if len(fitobj.CRmask[1]) > 0:
         for mb in fitobj.CRmask[1]:
             mask[(xdata >= fitobj.CRmask[0][mb]-1) & (xdata <= fitobj.CRmask[0][mb]+1)] = False
-
 
     if args.band == 'H':
         if np.int(order) in [13]:
@@ -141,41 +150,38 @@ def outplotter_23(parfit, fitobj, title, trk, inparam, args, step2or3, order):
 
     npars -= 3 # subtract 3 from npars total, 2 for linear/quadratic IP and 1 for RV_telluric
 
-    chi_new = chi*(len(fitobj.s[mask]) - len(parfit))/(len(fitobj.s[mask]) - npars)
+    chi_new = chi*(len(sdata[mask]) - len(parfit))/(len(sdata[mask]) - npars)
 
-    w = parfit[6] + parfit[7]*fitobj.x + parfit[8]*(fitobj.x**2.) + parfit[9]*(fitobj.x**3.)
-
-    cont = parfit[10] + parfit[11]*fitobj.x+ parfit[12]*(fitobj.x**2) + parfit[20]*(fitobj.x**3) + parfit[21]*(fitobj.x**4) + parfit[22]*(fitobj.x**5) + parfit[23]*(fitobj.x**6)
+    # Apply continuum adjustment
+    cont = parfit[10] + parfit[11]*xdata+ parfit[12]*(xdata**2) + parfit[20]*(xdata**3) + parfit[21]*(xdata**4) + parfit[22]*(xdata**5) + parfit[23]*(xdata**6)
     if fitobj.masterbeam == 'A':
         bucket = np.zeros_like(cont)
-        bucket[(fitobj.x >= (parfit[15]-parfit[16]/2)) & (fitobj.x <= (parfit[15]+parfit[16]/2))] = parfit[17]
-        bucket[(fitobj.x >= (parfit[15]+parfit[16]/2-parfit[18])) & (fitobj.x <= (parfit[15]+parfit[16]/2))] += parfit[19]
+        bucket[(xdata >= (parfit[15]-parfit[16]/2))         & (xdata <= (parfit[15]+parfit[16]/2))] = parfit[17]
+        bucket[(xdata >= (parfit[15]+parfit[16]/2-parfit[18])) & (xdata <= (parfit[15]+parfit[16]/2))] += parfit[19]
         cont -= bucket
-    cont *= fitobj.continuum
 
     fig, axes = plt.subplots(1, 1, figsize=(6,3), facecolor='white', dpi=300)
 
-    mask2 = np.ones_like(fitobj.x,dtype=bool)
-    
+    mask2 = np.ones_like(xdata,dtype=bool)
+
     if len(fitobj.CRmask[1]) > 0:
         for mb in fitobj.CRmask[1]:
             mask2[(xdata >= fitobj.CRmask[0][mb]-1) & (xdata <= fitobj.CRmask[0][mb]+1)] = False
 
-
     n = len(fitobj.mask)
 
     if n > 0:
-        widths = [fitobj.mask[0][0]-fitobj.x[0]]
+        widths = [fitobj.mask[0][0]-xdata[0]]
         for m in range(n-1):
             widths.append(fitobj.mask[m+1][0]-fitobj.mask[m][1])
-        widths.append(fitobj.x[-1]-fitobj.mask[n-1][1])
+        widths.append(xdata[-1]-fitobj.mask[n-1][1])
         gs = gridspec.GridSpec(1, n+1, width_ratios=widths)
         for m in range(n+1):
             ax0 = plt.subplot(gs[m])
 
 
-            ax0.plot(w,       fitobj.s,        '--', c='k',       lw=0.7, label='data',  alpha=.3)
-            ax0.plot(w[mask2],fitobj.s[mask2], '-',  c='k',       lw=0.7, label='data (emission removed)',  alpha=.8)
+            ax0.plot(w,       sdata,        '--', c='k',       lw=0.7, label='data',  alpha=.3)
+            ax0.plot(w[mask2],sdata[mask2], '-',  c='k',       lw=0.7, label='data (emission removed)',  alpha=.8)
             ax0.plot(w[mask2],fit[mask2],      '--', c='tab:red', lw=0.7, label='model', alpha=.8)
             ax0.plot(w[mask2],cont[mask2],     '--', c='tab:blue',  lw=0.7, label='cont', alpha=.8)
             kwargs = dict(transform=ax0.transAxes, color='k', clip_on=False,lw= 0.6)
@@ -183,7 +189,7 @@ def outplotter_23(parfit, fitobj, title, trk, inparam, args, step2or3, order):
                 ax0.tick_params(axis='both', labelsize=6, right=False, top=True, direction='in')
                 left = w[0]
                 right = parfit[6] + parfit[7]*fitobj.mask[m][0] + parfit[8]*(fitobj.mask[m][0]**2.) + parfit[9]*(fitobj.mask[m][0]**3.)
-                ax0.plot([right,right],[min(fitobj.s),max(fitobj.s)],'--k',lw=0.75)
+                ax0.plot([right,right],[min(sdata),max(sdata)],'--k',lw=0.75)
             elif m == n:
                 ax0.tick_params(axis='both', labelsize=6, left=False, right=True, top=True, direction='in')
                 ax0.set_yticklabels([])
@@ -194,7 +200,7 @@ def outplotter_23(parfit, fitobj, title, trk, inparam, args, step2or3, order):
                 ax0.tick_params(axis='both', labelsize=6, right=False, left=False,top=True, direction='in')
                 ax0.set_yticklabels([])
                 #ax0.vlines([left+1],'--k',lw=0.75)
-                ax0.plot([right,right],[min(fitobj.s),max(fitobj.s)],'--k',lw=0.75)
+                ax0.plot([right,right],[min(sdata),max(sdata)],'--k',lw=0.75)
                 left = parfit[6] + parfit[7]*fitobj.mask[m-1][1] + parfit[8]*(fitobj.mask[m-1][1]**2.) + parfit[9]*(fitobj.mask[m-1][1]**3.)
                 right = parfit[6] + parfit[7]*fitobj.mask[m][0] + parfit[8]*(fitobj.mask[m][0]**2.) + parfit[9]*(fitobj.mask[m][0]**3.)
 
@@ -215,8 +221,8 @@ def outplotter_23(parfit, fitobj, title, trk, inparam, args, step2or3, order):
 
     else:
         fig, axes = plt.subplots(1, 1, figsize=(6,3), facecolor='white', dpi=300)
-        axes.plot(w,       fitobj.s,        '--', c='k',       lw=0.7, label='data',  alpha=.3)
-        axes.plot(w[mask2],fitobj.s[mask2], '-',  c='k',       lw=0.7, label='data (emission removed)',  alpha=.8)
+        axes.plot(w,       sdata,        '--', c='k',       lw=0.7, label='data',  alpha=.3)
+        axes.plot(w[mask2],sdata[mask2], '-',  c='k',       lw=0.7, label='data (emission removed)',  alpha=.8)
         axes.plot(w[mask2],fit[mask2],      '--', c='tab:red', lw=0.7, label='model', alpha=.8)
         axes.plot(w[mask2],cont[mask2],     '--', c='tab:blue',  lw=0.7, label='cont', alpha=.8)
         axes.tick_params(axis='both', labelsize=6, right=True, top=True, direction='in')
@@ -230,3 +236,5 @@ def outplotter_23(parfit, fitobj, title, trk, inparam, args, step2or3, order):
         axes.legend(fontsize=5, edgecolor='white')
 
     fig.savefig(f'{inparam.outpath}/figs/main_step{step2or3}_{args.band}_{trk}/{title}.png', bbox_inches='tight', format='png', overwrite=True)
+
+    
