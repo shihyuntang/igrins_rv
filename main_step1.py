6c312b696cc64cc5cb34915d41d712c5baef00a0
sytang <sytang@lowell.edu>
2021-08-09 00:05:34 -0700

    bound new cut

--- main_step1.py
+++ main_step1.py
@@ -36,7 +36,7 @@ def MPinstB(args, inparam, jerp, orders, i):
     # Retrieve pixel bounds for where within each other significant telluric absorption is present.
     # If these bounds were not applied, analyzing some orders would give garbage fits.
     if args.band=='K':
-        if int(order) in [3, 13, 14]:
+        if int(order) in [3, 4, 13, 14]:
             bound_cut = inparam.bound_cut_dic[args.band][order]
         else:
             bound_cut = [150, 150]
--- main_step2.py
+++ main_step2.py
@@ -164,7 +164,7 @@ def rv_MPinst(args, inparam, orders, order_use, trk, step2or3, i):
     # Retrieve pixel bounds for where within each other significant telluric absorption is present.
     # If these bounds were not applied, analyzing some orders would give garbage fits.
     if args.band=='K':
-        if int(order) in [3, 13, 14]:
+        if int(order) in [3, 4, 13, 14]:
             bound_cut = inparam.bound_cut_dic[args.band][order]
         else:
             bound_cut = [150, 150]
