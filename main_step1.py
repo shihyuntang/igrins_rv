96a5988387bcaecaf9fe9315d2d0d79691099fb3
sytang <sytang@lowell.edu>
2021-08-10 09:23:31 -0700

    Update main_step1.py

--- main_step1.py
+++ main_step1.py
@@ -202,12 +202,28 @@ def MPinstB(args, inparam, jerp, orders, i):
 
     #-------------------------------------------------------------------------------
 
-    # Use quadratic blaze correction for order 13; cubic for orders 6, 14, 21; quartic for orders 16 and 22
+    # # Use quadratic blaze correction for order 13; cubic for orders 6, 14, 21; quartic for orders 16 and 22
+    # if args.band == 'H':
+    #     if int(order) in [13]:
+    #         dpars['cont'][20] = 0.; dpars['cont'][21] = 0.; dpars['cont'][22] = 0.; dpars['cont'][23] = 0.;
+    #     elif int(order) in [6,14,21]:
+    #         dpars['cont'][21] = 0.; dpars['cont'][22] = 0.; dpars['cont'][23] = 0.;
+    #     else:
+    #         pass
+    # else:
+    #     # if np.int(order) in [3]:
+    #     #     dpars['cont'][20] = 0.; dpars['cont'][21] = 0.; dpars['cont'][22] = 0.; dpars['cont'][23] = 0.;
+    #     if int(order) in [3,4,5]:
+    #         dpars['cont'][21] = 0.; dpars['cont'][22] = 0.; dpars['cont'][23] = 0.;
+    #     elif int(order) in [6]:
+    #         dpars['cont'][22] = 0.; dpars['cont'][23] = 0.;
+    #     else:
+    #         pass
+
+    # --- sytang modified ---
     if args.band == 'H':
         if int(order) in [13]:
             dpars['cont'][20] = 0.; dpars['cont'][21] = 0.; dpars['cont'][22] = 0.; dpars['cont'][23] = 0.;
-        elif int(order) in [6,14,21]:
-            dpars['cont'][21] = 0.; dpars['cont'][22] = 0.; dpars['cont'][23] = 0.;
         else:
             pass
     else:
