48acb8215697f8b128a10e52b41f603314390468
astahl7 <33397762+astahl7@users.noreply.github.com>
2021-09-10 16:14:15 -0500

    Update main_step3.py

--- main_step3.py
+++ main_step3.py
@@ -270,17 +270,17 @@ def rv_MPinst(args, inparam, orders, order_use, trk, step2or3, i):
 
         # Trim telluric template to data range +- 15 AA. If telluric template buffer is cut short because A0 lines didn't extend 
         # far past data range, cut data range accordingly.
-        satm_in = satm[(watm > np.min(wave_piece)*1e4 - 5) & (watm < np.max(wave_piece)*1e4 + 5)]
-        watm_in = watm[(watm > np.min(wave_piece)*1e4 - 5) & (watm < np.max(wave_piece)*1e4 + 5)]
+        satm_in = satm[(watm > np.min(wave_piece)*1e4 - 10) & (watm < np.max(wave_piece)*1e4 + 10)]
+        watm_in = watm[(watm > np.min(wave_piece)*1e4 - 10) & (watm < np.max(wave_piece)*1e4 + 10)]
 
-        s_piece	= s_piece[   (wave_piece*1e4 > np.min(watm_in)+5) & (wave_piece*1e4 < np.max(watm_in)-5)]
-        u_piece	= u_piece[   (wave_piece*1e4 > np.min(watm_in)+5) & (wave_piece*1e4 < np.max(watm_in)-5)]
-        x_piece	= x_piece[   (wave_piece*1e4 > np.min(watm_in)+5) & (wave_piece*1e4 < np.max(watm_in)-5)]
-        wave_piece = wave_piece[(wave_piece*1e4 > np.min(watm_in)+5) & (wave_piece*1e4 < np.max(watm_in)-5)]
+        s_piece	= s_piece[   (wave_piece*1e4 > np.min(watm_in)+10) & (wave_piece*1e4 < np.max(watm_in)-10)]
+        u_piece	= u_piece[   (wave_piece*1e4 > np.min(watm_in)+10) & (wave_piece*1e4 < np.max(watm_in)-10)]
+        x_piece	= x_piece[   (wave_piece*1e4 > np.min(watm_in)+10) & (wave_piece*1e4 < np.max(watm_in)-10)]
+        wave_piece = wave_piece[(wave_piece*1e4 > np.min(watm_in)+10) & (wave_piece*1e4 < np.max(watm_in)-10)]
 
         # Trim stellar template to data range +- 10 AA
-        mflux_in = inparam.mflux0[(inparam.mwave0 > np.min(wave_piece)*1e4 - 2.5) & (inparam.mwave0 < np.max(wave_piece)*1e4 + 2.5)]
-        mwave_in = inparam.mwave0[(inparam.mwave0 > np.min(wave_piece)*1e4 - 2.5) & (inparam.mwave0 < np.max(wave_piece)*1e4 + 2.5)]
+        mflux_in = inparam.mflux0[(inparam.mwave0 > np.min(wave_piece)*1e4 - 5) & (inparam.mwave0 < np.max(wave_piece)*1e4 + 5)]
+        mwave_in = inparam.mwave0[(inparam.mwave0 > np.min(wave_piece)*1e4 - 5) & (inparam.mwave0 < np.max(wave_piece)*1e4 + 5)]
 
         # Normalize continuum from A0 to flux scale of data
         continuum /= np.nanmedian(continuum)
