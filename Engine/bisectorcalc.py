e0c1863e4a0088900d1375cf24f2df97afc99264
astahl7 <33397762+astahl7@users.noreply.github.com>
2023-01-24 17:55:06 -0600

    Add files via upload

--- Engine/bisectorcalc.py
+++ Engine/bisectorcalc.py
@@ -84,7 +84,13 @@ class NightSpecs:
         if self.flag == 0:
             self.wave  = np.array(tbdata['WAVE'+str(order)],dtype=float)
             self.flux  = np.array(tbdata['FLUX'],dtype=float)
-            self.unc   = np.array(tbdata['UNC'],dtype=float)
+            if 'V827Tau' in inpath or 'Hubble4' in inpath:
+                self.unc   = np.array(tbdata['UNC'],dtype=float)
+            else:
+                self.unc   = np.array(tbdata['UNC2'],dtype=float)
+                if len(self.unc[np.isfinite(self.unc)]) == 0:
+                    self.flag = 1
+            self.stell = np.array(tbdata['STELLMOD'],dtype=float)
             self.night = str(night)
 
 
@@ -167,7 +173,9 @@ def peakfinderSpec(x,y,night,order,outpath,rv_sep,plot,tag,debug):
 
 def bisector(rv,cc,rv_out,outpath,night,order,debug):
 
-    points0 = np.concatenate((np.linspace(.4,.6,30),np.linspace(.7,.90,30)))
+    percenthighs = [0.70,0.9] #0.7-0.9
+    percentlows  = [0.40,0.6] #0.4-0.6
+    points0 = np.concatenate((np.linspace(percentlows[0],percentlows[1],30),np.linspace(percenthighs[0],percenthighs[1],30)))
 
     valleys = detect_peaks(cc,valley=True)
     valleysL = valleys[(rv[valleys] < rv_out - 7) & (cc[valleys] < 0.2)]
@@ -199,16 +207,13 @@ def bisector(rv,cc,rv_out,outpath,night,order,debug):
         fright = interp1d([ccrightover,ccrightunder],[rv[(cc == ccrightover)][0],rv[(cc == ccrightunder)][0]])
 
         sloperight = (rv[(cc == ccrightover)][0]-rv[(cc == ccrightunder)][0])/(ccrightover-ccrightunder)
-        #eright = np.mean([errs[(cc == ccrightover)],errs[(cc == ccrightunder)]])*sloperight
         slopeleft = (rv[(cc == ccleftover)][0]-rv[(cc == ccleftunder)][0])/(ccleftover-ccleftunder)
-        #eleft = np.mean([errs[(cc == ccleftover)],errs[(cc == ccleftunder)]])*slopeleft
 
-        if point < .5:
+        if point < percenthighs[0]:
             lowbis.append((fright(point)+fleft(point))/2.0)
         else:
             highbis.append((fright(point)+fleft(point))/2.0)
         bis.append((fright(point)+fleft(point))/2.0)
-        #errbis.append(np.mean([eleft,eright])/np.sqrt(2))
 
     if debug == True:
         plt.plot(rv,cc,alpha=1,color='blue')
@@ -222,7 +227,6 @@ def bisector(rv,cc,rv_out,outpath,night,order,debug):
 
     bi = np.mean(lowbis)-np.mean(highbis)
     bistd = np.sqrt( (np.std(lowbis)/np.sqrt(len(lowbis)))**2 + (np.std(highbis)/np.sqrt(len(highbis)))**2 )
-    #bistd = np.sqrt(  np.mean(errbis)**2 + (np.std(lowbis)/np.sqrt(len(lowbis)))**2 + (np.std(highbis)/np.sqrt(len(highbis)))**2 )
 
     return bi,bistd
 
@@ -245,27 +249,26 @@ def BIinst(refspec,orders,jerp,nights_use,args,inpath,outpath,logger,i):
         logger.warning('WARNING! {} order {} is flagged!'.format(newspec.filename,order))
         return night,np.nan,np.nan
 
-    if args.debug:
-        plt.figure(figsize=(16,10))
-        plt.plot(newspec.wave,newspec.flux,color='black',alpha=.5)
-        plt.plot(refspec.wave,refspec.flux,color='red',alpha=.5)
-        plt.savefig('{}/Spec_{}_{}.png'.format(outpath,newspec.night,order))
-        plt.clf()
-        plt.close()
-
 
     # Rebin refspec to uniform wave scale
     dw = np.median(np.diff(refspec.wave))
     nw = int( (refspec.wave[-1] - refspec.wave[0])/dw )
     uniwave  = np.linspace(refspec.wave[0],refspec.wave[-1],nw)
     uniflux  = rebin_jv(refspec.wave,refspec.flux,uniwave,False)
+    uniunc   = rebin_jv(refspec.wave,refspec.unc,uniwave,False)
+    unistell = rebin_jv(refspec.wave,refspec.stell,uniwave,False)
     uniwave = uniwave[1:-1]
     uniflux = uniflux[1:-1]
-    refspec.wave = uniwave; refspec.flux = uniflux;
+    uniunc  = uniunc[1:-1]
+    unistell = unistell[1:-1]
+
+    refspec.wave = uniwave; refspec.flux = uniflux; refspec.unc = uniunc; refspec.stell = unistell;
 
     # Rebin spectra to same wavelength scale
     rvrange = 100.
     newspec.flux = rebin_jv(newspec.wave,newspec.flux,refspec.wave,False)
+    newspec.unc  = rebin_jv(newspec.wave,newspec.unc,refspec.wave,False)
+    newspec.stell = rebin_jv(newspec.wave,newspec.stell,refspec.wave,False)
 
     # Now split treatment:
     refspec1 = deepcopy(refspec); refspec2 = deepcopy(refspec);
@@ -274,39 +277,60 @@ def BIinst(refspec,orders,jerp,nights_use,args,inpath,outpath,logger,i):
     # for pixel-wise correlation, cut both to be on same non-overinterpolated wave scale
     newspec2.flux = newspec2.flux[(refspec.wave > newspec.wave[0]) & (refspec.wave < newspec.wave[-1])]
     refspec2.flux = refspec2.flux[(refspec.wave > newspec.wave[0]) & (refspec.wave < newspec.wave[-1])]
+    newspec2.unc  = newspec2.unc[(refspec.wave > newspec.wave[0]) & (refspec.wave < newspec.wave[-1])]
+    refspec2.unc  = refspec2.unc[(refspec.wave > newspec.wave[0]) & (refspec.wave < newspec.wave[-1])]
+    newspec2.stell  = newspec2.stell[(refspec.wave > newspec.wave[0]) & (refspec.wave < newspec.wave[-1])]
+    refspec2.stell  = refspec2.stell[(refspec.wave > newspec.wave[0]) & (refspec.wave < newspec.wave[-1])]
     newspec2.wave = refspec2.wave[(refspec.wave > newspec.wave[0]) & (refspec.wave < newspec.wave[-1])]
     refspec2.wave = refspec2.wave[(refspec.wave > newspec.wave[0]) & (refspec.wave < newspec.wave[-1])]
-    refspec2.flux[(refspec2.flux > 1.03)] = np.nan
-    newspec2.flux[(newspec2.flux > 1.03)] = np.nan
+    refspec2.flux[(refspec2.flux > 1.03) | (refspec2.flux < 0)] = np.nan
+    newspec2.flux[(newspec2.flux > 1.03) | (newspec2.flux < 0)] = np.nan
 
-    ### COMMENTED OUT ALL THE PYASTRO STUFF
+    refspec2.unc[(refspec2.unc > 1) | (refspec2.unc < 0)] = np.nan
+    newspec2.unc[(newspec2.unc > 1) | (newspec2.unc < 0)] = np.nan
 
-    # For wavelength-wise correlation, cut edges of one spectrum for shifts from the other
-    #When template left edge is shifted 100 km/s to the right, it must still be less than left edge of data
-    #When template right edge is shifted 100 km/s to the left, it must still be more than right edge of data
-    #newspec1.flux = newspec1.flux[(refspec1.wave > newspec1.wave[0]) & (refspec1.wave < newspec1.wave[-1])]
-    #newspec1.wave = refspec1.wave[(refspec1.wave > newspec1.wave[0]) & (refspec1.wave < newspec1.wave[-1])]
-    #newspec1.flux = newspec1.flux[(newspec1.wave > refspec1.wave[0]*(1+rvrange/c)) & (newspec1.wave < refspec1.wave[-1]*(1-rvrange/c))]
-    #newspec1.wave = newspec1.wave[(newspec1.wave > refspec1.wave[0]*(1+rvrange/c)) & (newspec1.wave < refspec1.wave[-1]*(1-rvrange/c))]
+    ind = np.where(abs(newspec2.flux-refspec2.flux) > 0.1 )[0]
+    refspec2.flux[ind] = np.nan
+    newspec2.flux[ind] = np.nan
+
+    refspec2.flux[refspec2.stell > 0.9975] = 1.
+    newspec2.flux[newspec2.stell > 0.9975] = 1.
+
+    #matplotlib.use('Qt5Agg')
+    #plt.plot(newspec2.wave,newspec2.flux)
+    #plt.plot(newspec2.wave,newspec2.unc,color='red')
+    #plt.show()
+
+    if args.debug:
+        #matplotlib.use('Qt5Agg')
+        plt.figure(figsize=(16,10))
+        plt.plot(newspec2.wave,newspec2.flux,color='black',alpha=.5)
+        plt.plot(refspec2.wave,refspec2.flux,color='red',alpha=.5)
+        plt.ylim(0.7,1.2)
+        #plt.show()
+        plt.savefig('{}/Spec_{}_{}.png'.format(outpath,newspec.night,order))
+        plt.clf()
+        plt.close()
 
     rv_sep  = c*np.median(np.diff(newspec2.wave))/np.median(newspec2.wave)
 
-    #rv10, cc10 = crosscorrRV( newspec1.wave, newspec1.flux, refspec1.wave, refspec1.flux,  -100, 100, 0.01, skipedge=0)
-    #rvguess = rv10[np.argmax(cc10)]
-    #rv1, cc1 = crosscorrRV(  newspec1.wave, newspec1.flux, refspec1.wave, refspec1.flux, rvguess-10, rvguess+10, 0.001, skipedge=0)
     rv2, cc2 = correlator(refspec2.flux,newspec2.flux,rv_sep)
-
-    #rv_out0,rv0,cc0 = peakfinderSpec(rv10,cc10,night,order,outpath,rv_sep,args.plotfigs,'pyAwide',args.debug)
-    #rv_out1,rv1,cc1 = peakfinderSpec(rv1,cc1,night,order,outpath,rv_sep,args.plotfigs,'pyA',args.debug)
     rv_out2,rv2,cc2 = peakfinderSpec(rv2,cc2,night,order,outpath,rv_sep,args.plotfigs,'uni',args.debug)
-
-    #if abs(rv_out1-rv_out2) > 1:
-    #    logger.info(f'WARNING! {night} order {order} shows CC RV disagreement of {round(abs(rv_out1-rv_out2),2)} km/s!')
-    #    return night,np.nan,np.nan
-
-    #rv_out = rv_out1
-    #rv = rv1.copy(); cc = cc1.copy(); # ONLY USE PYA CC
-
     bi,bistd = bisector(rv2,cc2,rv_out2,outpath,night,order,args.debug)
 
-    return night,bi,bistd
+    if abs(bi) > 1:
+        return night, np.nan, np.nan
+
+    fakebis = []; fakervs = [];
+    for nmc in range(100):
+        reffluxfake = np.random.normal(refspec2.flux,refspec2.unc)
+        newfluxfake = np.random.normal(newspec2.flux,newspec2.unc)
+
+        rv2fake, cc2fake = correlator(reffluxfake,newfluxfake,rv_sep)
+        rv_out2fake,rv2fake,cc2fake = peakfinderSpec(rv2fake,cc2fake,night,order,outpath,rv_sep,False,'uni',False)
+        bifake,bistdfake = bisector(rv2fake,cc2fake,rv_out2fake,outpath,night,order,False)
+        fakervs.append(rv_out2fake); fakebis.append(bifake);
+
+    bierr = np.sqrt(bistd**2 + (np.std(fakebis)/10)**2)
+
+    return night,bi,bierr
